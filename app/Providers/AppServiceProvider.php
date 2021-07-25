a089a5a1b2414c4e3b86153b7871dc7a8385ab7d
merika427 <erikais427gmail.com>
2021-07-25 09:22:57 +0900

    Laravel開発入門 c1~c7 migration,モデル,ルーティングなど

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;  //dbせってい
+use Illuminate\Support\Facades\URL;    //この行を追加 [本のため]
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,5 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191); //dbせってい
+        URL::forceScheme('https');          //boot()内に、この行を追加 [本のため]
     }
 }
