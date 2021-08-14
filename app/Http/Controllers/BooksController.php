e09246b6255abc0264d3bd958d0946a2d75feeea
merika427 <erikais427gmail.com>
2021-08-15 00:51:29 +0900

    チャプター20 デバックツール表示のddd [現在コメントアウト]

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -31,6 +31,8 @@ class BooksController extends Controller
         // $books = Book::orderBy('created_at', 'asc')->get();
         // $books = Book::orderBy('created_at', 'asc')->paginate(3);
         $books = Book::where('user_id',Auth::user()->id)->orderBy('created_at', 'asc')->paginate(3);
+
+        // ddd($books);
         return view('books', [
             'books' => $books
         ]);
